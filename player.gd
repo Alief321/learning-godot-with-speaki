129c02e278869cfb5759e5bf3d92108d55f6e5da
Alief Zulvan <depanb187@gmail.com>
2025-12-16 21:15:11 +0700

    finished the game dev

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -12,20 +13,41 @@ func _ready() -> void:
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	var moving_horizontal = false
+	var moving_vertical = false
+	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		moving_horizontal = true
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		moving_horizontal=true
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		moving_vertical=true
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		moving_vertical=true
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+		
+		# ===== SOUND CONTROL =====
+	if moving_horizontal:
+		$FlySound.stop()
+		if not $WalkSound.playing:
+			$WalkSound.play()
+	elif moving_vertical:
+		$WalkSound.stop()
+		if not $FlySound.playing:
+			$FlySound.play()
+	else:
+		$WalkSound.stop()
+		$FlySound.stop()
 		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
@@ -39,12 +61,12 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "cry"
 		
+		
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
 
-signal hit
 func _on_body_entered(_body):
 	hide() # Player disappears after being hit.
 	hit.emit()
